d8814e46d35e0c866fec1f99f80a671f50e9c4e8
AdinDorf <adin@dorffamily.com>
2024-09-17 15:46:38 -0700

    Did a bunch of stuff read the code idiot

--- library.js
+++ library.js
@@ -2,28 +2,66 @@ const myLibrary = [];
 const cardList = document.querySelector(".card-list");
 
 //Book Constructor
-function Book (title, author, pages, read) {
+function Book (title, author, wordCount, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.wordCount = wordCount;
     this.read = read;
-
     this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    }
+        return this.title + this.author + this.wordCount + this.read;
+    };
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, wordCount, read) {
+    let book = new Book(title, author, wordCount, read);
     myLibrary.push(book);
+
+    console.log("Adding " + book.info());
+
+    let newBook = document.createElement("div")
+    newBook.classList.add("book");
+    cardList.appendChild(newBook);
+
+    let newBookTitle = document.createElement('h2');
+    newBook.appendChild(newBookTitle);
+    newBookTitle.textContent = book.title;
+   
+    let newBookAuthor = document.createElement('p');
+    newBook.appendChild(newBookAuthor);
+    newBookAuthor.textContent = book.author;
+
+    let newBookWordCount = document.createElement('p');
+    newBook.appendChild(newBookWordCount);
+    newBookWordCount.textContent = book.wordCount;
+
+    let newBookRead = document.createElement('p');
+    newBook.appendChild(newBookRead);
+    newBookRead.textContent = book.read;
 }
 
-function updateDisplay(){
+function InitializeDisplay(){
     myLibrary.forEach(book => {
         console.log("Adding " + book.info());
-        let newBook = document.createElement("li")
+
+        let newBook = document.createElement("div")
         newBook.classList.add("book");
         cardList.appendChild(newBook);
+
+        let newBookTitle = document.createElement('h2');
+        newBook.appendChild(newBookTitle);
+        newBookTitle.textContent = book.title;
+       
+        let newBookAuthor = document.createElement('p');
+        newBook.appendChild(newBookAuthor);
+        newBookAuthor.textContent = book.author;
+
+        let newBookWordCount = document.createElement('p');
+        newBook.appendChild(newBookWordCount);
+        newBookWordCount.textContent = book.wordCount;
+
+        let newBookRead = document.createElement('p');
+        newBook.appendChild(newBookRead);
+        newBookRead.textContent = book.read;
     });
 }
 
@@ -31,5 +69,15 @@ function updateDisplay(){
 // Create a book manually
 addBookToLibrary("The Way of Kings", "Brandon Sanderson", 383389, "read");
 addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
+addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
+addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
+addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
+addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
+addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
+addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
+addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
+addBookToLibrary("The Crippled God", "Steven Erikson", 376000, "unread");
 
-updateDisplay();
+myLibrary.forEach(book => {
+    console.log("Books: " + book.info());
+});
